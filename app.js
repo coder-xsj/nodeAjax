2b016a6eddc000f8185ec159c60ba39872e0ac26
徐大帅 <fibonacci701408733@163.com>
2020-09-28 01:06:39 +0800

    学习了get和post请求

--- app.js
+++ app.js
@@ -4,24 +4,29 @@ const express = require('express');
 const path = require('path');
 // 创建web服务器
 const app = express();
-
+// 为了获取post参数
+const bodyParser = require('body-parser');
 // 静态资源访问服务功能
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(bodyParser());
 // 设置模板引擎和视图位置
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-
+// 对应01-ajax
 app.get('/first', (req, res) => {
   // res.send('Hello Ajax');
   res.send({'name': 'xsj'});
 });
-
-app.get('/get', ((req, res) => {
+// 对应03-传递get参数
+app.get('/get', (req, res) => {
     res.send(req.query);
+});
 
-}))
+// 对应04-传递post参数
+app.post('/post', (req, res) => {
+    res.send(req.body) //req.body获取post提交的数据
+});
 
 
 
