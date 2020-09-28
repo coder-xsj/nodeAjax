9e93c44a8e55bddee3631dab9eeb5533b531db31
徐大帅 <fibonacci701408733@163.com>
2020-09-28 16:49:39 +0800

    学习了处理error事件

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const app = express();
 const bodyParser = require('body-parser');
 // 静态资源访问服务功能
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser());
+// app.use(bodyParser());
+app.use(bodyParser.json());
 // 设置模板引擎和视图位置
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -28,6 +29,22 @@ app.post('/post', (req, res) => {
     res.send(req.body) //req.body获取post提交的数据
 });
 
+// 对应05-传递post参数json格式
+app.post('/json', (req, res) => {
+    res.send(req.body);
+});
+
+// 06-onreadystate事件
+app.get('/readystate', (req, res) => {
+    res.send("hello readyState");
+});
+
+app.get('/error', (req, res) => {
+    //打印一个不存在的变量 自动返回500 服务器错误
+    // console.log(exist);
+    // 设置响应码为400
+   res.status(400).send('server not found!');
+});
 
 
 // app.listen(3000);
